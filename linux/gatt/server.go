cce652d06a008c9012674647a4b2657efd4c121f
Melvin Tu <melvinto@gmail.com>
2020-02-04 14:55:44 +0800

    hide logs

--- linux/gatt/server.go
+++ linux/gatt/server.go
@@ -1,7 +1,6 @@
 package gatt
 
 import (
-	"log"
 	"sync"
 
 	"github.com/melvinto/ble"
@@ -93,11 +92,11 @@ func defaultServicesWithHandler(name string, handler ble.NotifyHandler) []*ble.S
 }
 
 func defaultHanderFunc(r ble.Request, n ble.Notifier) {
-	log.Printf("TODO: indicate client when the services are changed")
+	//	log.Printf("TODO: indicate client when the services are changed")
 	for {
 		select {
 		case <-n.Context().Done():
-			log.Printf("count: Notification unsubscribed")
+			//			log.Printf("count: Notification unsubscribed")
 			return
 		}
 	}
